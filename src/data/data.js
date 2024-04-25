0fac541d5a9259c5437c433c78134d8a0c4129d2
RamirezAgus <rlagustin0@gmail.com>
2024-04-25 00:06:46 -0300

    add slider products

---
+++ src/data/data.js
@@ -0,0 +1,42 @@
+export const blogs = [
+    {
+        id: 1,
+		title: 'HP LaserJet Tank MFP 1602w',
+		description:
+			'Capacidades empresariales con tóner precargado para hasta 5000 páginas y un costo por página muy bajo',
+		category: 'Impresoras',
+		brand: 'HP',
+    },
+    {
+        id: 2,
+		title: 'PTE300',
+		description:
+			'Sistema de etiquetado independiente de bajo costo',
+		category: 'Rotuladoras',
+		brand: 'Brother',
+    },
+    {
+        id: 3,
+		title: 'PTP700',
+		description:
+			'Impresora de etiquetas de escritorio versátil',
+		category: 'Rotuladoras',
+		brand: 'Brother',
+    },
+    {
+        id: 4,
+		title: 'QL810W',
+		description:
+			'Se conecta fácilmente a su red inalámbrica',
+		category: 'Etiquetadoras',
+		brand: 'Brother',
+    },
+    {
+        id: 5,
+		title: 'QL1100',
+		description:
+			'Imprime etiquetas de formato ancho de hasta 100 mm de ancho',
+		category: 'Etiquetadoras',
+		brand: 'Brother',
+    },
+]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import Carrousel from '../components/carrousel';
 import Main from '../components/main';
 import Parterns from '../components/partners';
 import Map from '../components/map';
+import Products from '../components/products';
 
 export default function Home(){
     return(
         <div className='bg-cyan-500'>
             <Carrousel/>
             <Main />
+            <Products/>
             <Parterns/>
             <Map/>
         </div>
